f7aca2111992f2ce5905e80d80a0f1f37677972d
Richard Blythe <unity3software@gmail.com>
2016-04-20 12:15:53 -0400

    added custom pods audio function

---
+++ bundled/unity3-pods-audio.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: R_Blythe
+ * Date: 4/20/2016
+ * Time: 12:12 PM
+ */
+
+function unity3_pods_audio($ID) {
+    global $post;
+    $pods = pods( 'sermon', $ID );
+    $attr = array(
+        'preload' => 'none',
+        'style'   => 'display: inline-block; width: 50%; visibility: visible'
+    );
+    if ($mp3 = $pods->field('mp3')) {
+        $attr['mp3'] = wp_get_attachment_url($mp3['ID']);
+    }
+    if ($ogg = $pods->field('ogg')) {
+        $attr['ogg'] = wp_get_attachment_url($ogg['ID']);
+    }
+    echo wp_audio_shortcode( $attr );
+}
