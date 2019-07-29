6e766d2e55cbb34d037f31a8be6412ce76f4734f
Richard Blythe <unity3software@gmail.com>
2019-07-29 15:24:15 -0400

    Improvements for Unity3_Audio

--- includes/post-types/class-audio.php
+++ includes/post-types/class-audio.php
@@ -20,8 +20,68 @@ class Unity3_Audio extends Unity3_Post_Group {
 		//The client should be a be paying for the audio add-on package
 		add_filter( 'upload_mimes',  array( &$this, 'unity3_aws_storage_mime'), 12, 1 );
 
+		add_shortcode('unity3_audio', array(&$this, 'shortcode') );
+
 
 		return true;
+	}
+
+	public function shortcode( $atts ) {
+		$atts = shortcode_atts( array(
+			'post' => '',
+			'group' => '',
+			'show_title' => true
+		), $atts );
+
+
+		$post = null;
+		$args = array(
+			'post_type'      => $this->GetPostType(),
+			'numberposts' => '1',
+			'orderby'        => 'date',
+			'order'          => 'DESC',
+		);
+
+
+		//Get the latest post in the specified group...
+		if ( isset($atts['group']) && !empty($atts['group']) ) {
+			$args['tax_query'] = array(
+				array(
+					'taxonomy' => $this->GetTaxonomy(),
+					'field' => 'slug',
+					'terms' => $atts['group']
+				)
+			);
+		}
+
+		if ( isset($atts['post']) && !empty($atts['post']) ) {
+			$args['post__in'] = $atts['post'];
+		}
+
+		//get the post...
+		$posts = get_posts( $args );
+
+		if (isset($posts) && count($posts) == 1 ) {
+
+			$post_id = $posts[0]->ID;
+			$field = get_field('audio', $post_id);
+
+			$attr = array(
+				'src'      => $field['url'],
+				'loop'     => '',
+				'autoplay' => '',
+				'preload' => 'none'
+			);
+
+			$title = '';
+			if ( isset($atts['show_title']) && true == $atts['show_title'] ) {
+				$title = '<h2>' . get_the_title( $post_id ) . '</h2>';
+			}
+
+			return '<div class="unity3_audio">' . $title . wp_audio_shortcode( $attr ) . '</div>';
+		}
+
+		return '';
 	}
 
 	public function unity3_aws_storage_mime( $types ) {
@@ -57,6 +117,8 @@ class Unity3_Audio extends Unity3_Post_Group {
 		);
 	}
 }
+
+
 
 ////*************************
 ////       Register
