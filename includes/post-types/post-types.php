d311c48def9131284f4c56dd42fd224c0e2e80cc
Richard Blythe <unity3software@gmail.com>
2019-08-09 11:39:37 -0400

    Fixed renaming issue in Service Times

--- includes/post-types/post-types.php
+++ includes/post-types/post-types.php
@@ -33,7 +33,7 @@ class Unity3_Post_Types {
 		require_once (Unity3::$dir . 'includes/post-types/class-post-group.php');
 		require_once (Unity3::$dir . 'includes/post-types/class-galleries.php');
 		require_once (Unity3::$dir . 'includes/post-types/class-slides.php');
-		require_once (Unity3::$dir . 'includes/post-types/class-services.php');
+		require_once (Unity3::$dir . 'includes/post-types/class-service-time.php');
 		require_once (Unity3::$dir . 'includes/post-types/class-audio.php');
 
 		do_action('unity3/post_types/init' );
