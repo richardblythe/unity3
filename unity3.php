60627587b28c71ae9c7961ddabd6197b3d29b93e
richardblythe <richardblythe84@gmail.com>
2021-09-09 13:12:18 -0400

    More updates for centering a YouTube embed

--- unity3.php
+++ unity3.php
@@ -3,7 +3,7 @@
     Plugin Name: Unity 3 Software
     Plugin URI: http://www.unity3software.com/
     Description: Core components for building Wordpress sites that are easy for the client to use.
-    Version: 2.7.3
+    Version: 2.7.4
     Author: Richard Blythe
     Author URI: http://unity3software.com/richardblythe
     GitHub Plugin URI: https://github.com/richardblythe/unity3
